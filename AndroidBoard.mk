ec0d35b6139b2ef0aee5c9cca2de66657e19bcd7
Unknown <github@nohup.xyz>
2018-09-08 20:54:58 +0300

    Switch to in-tree kernel builds

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,17 +1,11 @@
-#
-# Copyright 2018 The Android Open-Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+KERNEL_DEFCONFIG := wahoo_defconfig
+KERNEL_DIR := kernel/google/wahoo
+TARGET_KERNEL_APPEND_DTB := true
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
--include vendor/google_devices/taimen/proprietary/AndroidBoardVendor.mk
+include $(TOP)/$(KERNEL_DIR)/AndroidKernel.mk
+
+.PHONY: $(PRODUCT_OUT)/kernel
+$(PRODUCT_OUT)/kernel: $(TARGET_PREBUILT_KERNEL)
+	cp $(TARGET_PREBUILT_KERNEL) $(PRODUCT_OUT)/kernel
